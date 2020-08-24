76872a9abf39a9700974e4139ddd21899f5f4afe
EihctirSinned <69522123+EihctirSinned@users.noreply.github.com>
2020-08-24 19:53:42 -0300

    Update ArrayList.php

--- ArrayList.php
+++ ArrayList.php
@@ -41,4 +41,51 @@
       }
       return $size;
     }
+   
+   public function isEmpty()
+   { 
+    if (count($this->array) == 0) return true; 
+     else return false;     
+   }
+
+   public function sort()
+   { 
+    sort($this->array); 
+   }
+
+   public function getAll() 
+   { 
+    return $this->array; 
+   }
+
+   public function isInteger($toCheck)
+   { 
+    return preg_match('/^[0-9]*$/', $toCheck); 
+   }
+
+   public function addToPos($index, $obj)
+   {         
+    if ($this->isInteger($index))  
+     $this->array[$index] = $obj;
+    else  
+     die("ERROR in ArrayList.addToPos <br> Integer value required"); 
+   }
+
+   public function get($index)
+   { 
+    if ($this->isInteger($index)) 
+     return $this->array[$index]; 
+    else  
+     die("ERROR in ArrayList.get <br> Integer value required"); 
+   }
+
+   public function addAll($arr)
+   { 
+    $this->array = array_merge($this->array, $arr);
+   }
+
+   public function destroy()
+   {
+    $this->array = array();
+   }
   }
